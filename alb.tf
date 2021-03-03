e86e99ab03ec8c742d2d5af673ef83184db4eb84
Nic <nic.ceynowa@gmail.com>
2021-03-03 18:01:25 -0500

    working on adding in acm

--- alb.tf
+++ alb.tf
@@ -24,35 +24,35 @@ resource "aws_route53_record" "alb_endpoint" {
 }
 
 #going to add in all the route53 stuff here, including ACM stuff.
-#resource "aws_acm_certificate" "cert_request" {
-#  domain_name               = var.domain_name
-#  validation_method         = "DNS"
+resource "aws_acm_certificate" "cert_request" {
+  domain_name               = var.domain_name
+  validation_method         = "DNS"
 
-#  tags = {
-#    Name : var.domain_name
-#  }
+  tags = {
+    Name : var.domain_name
+  }
 
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-#resource "aws_route53_record" "validation_record" {
-#  for_each = {
-#    for dvo in aws_acm_certificate.cert_request.domain_validation_options : dvo.domain_name => {
-#      name   = dvo.resource_record_name
-#      record = dvo.resource_record_value
-#      type   = dvo.resource_record_type
-#    }
-#  }
+resource "aws_route53_record" "validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert_request.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#  allow_overwrite = true
-#  name            = each.value.name
-#  records         = [each.value.record]
-#  ttl             = var.ttl
-#  type            = each.value.type
-#  zone_id         = aws_route53_zone.main_zone.zone_id
-#}
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  #ttl             = var.ttl
+  type            = each.value.type
+  zone_id         = var.zone_id
+}
 
 #resource "aws_acm_certificate_validation" "certificate_validation" {
 #  certificate_arn         = aws_acm_certificate.cert_request.arn
@@ -101,6 +101,24 @@ resource "aws_alb_listener" "web_app_http" {
   load_balancer_arn = aws_alb.app_alb.arn
   port              = "80"
   protocol          = "HTTP"
+  depends_on        = [aws_alb_target_group.api_target_group]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  default_action {
+    target_group_arn = aws_alb_target_group.api_target_group.arn
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener" "web_app_https" {
+  #count = local.is_only_http ? 1 : 0
+  count = var.use_cert ? 1 : 0
+  load_balancer_arn = aws_alb.app_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
   depends_on        = [aws_alb_target_group.api_target_group]
 
   lifecycle {
